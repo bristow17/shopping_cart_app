8aa4900ef990af4873b154ae53d417dbcaa00b45
bristow17 <bristowgrorge17@gmail.com>
2024-06-08 06:41:21 +0530

    added view

---
+++ src/components/SearchProducts.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const SearchProducts = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="label form-label">ID</label>
+                                <input type="text" className="input form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <button className="btn btn-dark">SEARCH</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+    </div>
+  )
+}
+
+export default SearchProducts
